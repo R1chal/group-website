b806fd09360d1f332083426c5dfc0f42872249af
Richal <lirichal3@gmail.com>
2026-04-29 20:01:10 +0800

    Add GitHub Actions deploy to Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://r1chal.github.io',
+  base: '/group-website',
   vite: {
     plugins: [tailwindcss()]
   }
